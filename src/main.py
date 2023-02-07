c910ba082cc9aee6a5aefab2bd5442aa7db0fcea
jwke21 <jakevm21@gmail.com>
2023-02-06 22:48:41 -0800

    Began defining main function

---
+++ src/main.py
@@ -0,0 +1,44 @@
+from pca import *
+from knn import *
+
+
+def main():
+    #################### PART 1 ####################
+
+    #################### PART 2 ####################
+
+    #################### PART 3 ####################
+
+    #################### PART 4 ####################
+    X_raw, y_raw = load_uci_data()
+    # Split data into 75% training data and 25% test data
+    X_train, X_test, y_train, y_test = train_test_split(X_raw, y_raw, test_size=0.25)
+
+    # Classify data set using KNN (Features are already whitened)
+    n_neigh, score = find_optimal_num_neighbors(X_train, X_test, y_train, y_test)
+    knn = KNeighborsClassifier(n_neighbors=n_neigh)
+
+    # Execute PCA on data
+    _, _, eigenvalues, _, proj_data = pca(X_raw, normalize=True)
+    # Keep enough eigenvectors to explain 90% of the data
+    X_reduced = reduce_dimensions(proj_data, eigenvalues, explained_variance=0.9)
+    # Split the reduced-dimension data into train-test sets
+    X_train, X_test, y_train, y_test = train_test_split(X_reduced, y_raw, test_size=0.25)
+    # Train the new classifier on the reduced-dimension data
+    n_neigh, score = find_optimal_num_neighbors(X_train, X_test, y_train, y_test)
+    knn = KNeighborsClassifier(n_neighbors=n_neigh)
+
+    # Keep enough eigenvectors to explain 80% of the data (Extension)
+    X_reduced = reduce_dimensions(proj_data, eigenvalues, explained_variance=0.8)
+    X_train, X_test, y_train, y_test = train_test_split(X_reduced, y_raw, test_size=0.25)
+    n_neigh, score = find_optimal_num_neighbors(X_train, X_test, y_train, y_test)
+
+    # Keep enough eigenvectors to explain 70% of the data (Extension)
+    X_reduced = reduce_dimensions(proj_data, eigenvalues, explained_variance=0.7)
+    X_train, X_test, y_train, y_test = train_test_split(X_reduced, y_raw, test_size=0.25)
+    n_neigh, score = find_optimal_num_neighbors(X_train, X_test, y_train, y_test)
+
+
+
+if __name__ == "__main__":
+    main()
